5a17a914624b998413132b09f5eea94ba1519c53
kazuki-0418 <kazuki.castle0418@gmail.com>
2024-11-21 09:31:25 -0800

    Exercise is done

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-// YOUR JS CODE HERE
+const temperature = document.getElementById("temperature");
+const windSpeed = document.getElementById("wind-speed");
+const timezone = document.getElementById("timezone");
+const time = document.getElementById("time");
+const loading = document.getElementById("loading");
+
+(function getWeather() {
+  fetch(
+    "https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&current=temperature_2m,is_day,rain,showers,wind_speed_10m&timezone=auto&forecast_days=1"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const current = data.current;
+      const currentUnits = data.current_units;
+
+      temperature.innerHTML =
+        current.temperature_2m + currentUnits.temperature_2m;
+      windSpeed.innerHTML =
+        current.wind_speed_10m + currentUnits.wind_speed_10m;
+      timezone.innerHTML = data.timezone;
+      time.innerHTML = new Date(current.time).toLocaleString();
+    })
+    .finally(() => {
+      loading.style.display = "none";
+    });
+})();
